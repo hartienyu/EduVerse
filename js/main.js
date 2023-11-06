ab82ae06255fd6e590c97e5d9ef57c8dead738fc
Har Tien Yu <hartienyu@gmail.com>
2023-11-07 00:51:44 +0800

    Modify the 404 page. Optimize the back to top animation speed

--- js/main.js
+++ js/main.js
@@ -63,7 +63,7 @@
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 100, 'easeInOutExpo');
         return false;
     });
 
